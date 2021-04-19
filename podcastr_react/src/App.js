8496e327150bfc90399b8512160e497484addaf0
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-04-19 20:20:32 -0300

    Conceito Propriedade Children

--- podcastr_react/src/App.js
+++ podcastr_react/src/App.js
@@ -3,10 +3,10 @@ import Button from "./Button";
 function App() {
   return (
     <>
-      <Button title="Botao 1"></Button>
-      <Button title="Botao 2"></Button>
-      <Button title="Botao 3"></Button>
-      <Button title="Botao 4"></Button>  
+      <Button> Botao 1</Button>
+      <Button> Botao 2</Button>
+      <Button> Botao 3</Button>
+      <Button> Botao 4</Button>  
     </>
   );
 }
