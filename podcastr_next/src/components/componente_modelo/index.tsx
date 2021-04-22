9ba9ec1d363a21708f9a92823135c0a3d6e6f23d
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-04-22 16:54:22 -0300

    Criado componente modelo para ser utilizado para gerar outros componentes

---
+++ podcastr_next/src/components/componente_modelo/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import styles from './style.module.scss';
+ 
+export function ComponenteModelo () {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  );
+}
+
