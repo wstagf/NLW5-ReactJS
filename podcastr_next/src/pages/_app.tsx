7a9f4190e3a9929aec69c7ff0d75df21482bb045
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-04-22 16:55:54 -0300

    movido o header para o app para ficar em todas as paginas

--- podcastr_next/src/pages/_app.tsx
+++ podcastr_next/src/pages/_app.tsx
@@ -1,7 +1,14 @@
+import React from 'react'
+import { Header } from '../components/header'
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Header/>
+      <Component {...pageProps} />
+    </div>  
+  )  
 }
 
 export default MyApp
--- podcastr_next/src/pages/index.tsx
+++ podcastr_next/src/pages/index.tsx
@@ -1,7 +1,5 @@
-import { Header } from "../components/header";
-
 export default function Home() {
   return (
-    <Header/>
+   <h1>Index</h1>
   )
 }
