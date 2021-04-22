498e6e7406709b73358b096c5e3e652873f805d0
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-04-22 16:46:26 -0300

    instalado bibliteca de data e colocando na aplicacao

--- podcastr_next/src/components/header/index.tsx
+++ podcastr_next/src/components/header/index.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import styles from './style.module.scss';
-
+import format from "date-fns/format";
+import ptBR from "date-fns/locale/pt-BR"
 export interface IHeaderProps {
 }
 
 export function Header (props: IHeaderProps) {
+    const currentDate = format(new Date(), 'EEEEE, d, MMMM');
+
   return (
     <header className={styles.headerContainer}>
       <img src="/logo.svg"></img>
