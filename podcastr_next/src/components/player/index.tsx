d3bcc9e8e0c3851095fa6475db5ba53499729074
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-04-22 17:43:11 -0300

    estilizado player buttons

--- podcastr_next/src/components/player/index.tsx
+++ podcastr_next/src/components/player/index.tsx
@@ -12,7 +12,33 @@ export function Player () {
       <div className={styles.emptyPlayer}>
         <strong>Selecione um podcast para ouvir</strong>
       </div>
-      <footer></footer>
+      <footer className={styles.empty}>
+        <div className={styles.progress}>
+          <span>00:00</span>
+          <div className={styles.slider}>
+            <div className={styles.emptySlider}/>
+          </div>
+          
+          <span>00:00</span>
+        </div>
+        <div className={styles.button}>
+          <button type="button">
+            <img src="/shuffle.svg" alt="Embaralhar"></img>
+          </button>
+          <button type="button">
+            <img src="/play-previous.svg" alt="Tocar anterior"></img>
+          </button>
+          <button type="button" className={styles.playButton}>
+            <img src="/play.svg" alt="Tocar"></img>
+          </button>
+          <button type="button">
+            <img src="/play-next.svg" alt="Tocar próxima"></img>
+          </button>
+          <button type="button">
+            <img src="/repeat.svg" alt="Repetir"></img>
+          </button>
+        </div>
+      </footer>
     </div>
   );
 }
