199da2079f8ab2a029ab0e250ff73de2766c503d
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-04-23 17:10:40 -0300

    configurado o request para a api em modo SSR

--- podcastr_next/src/pages/index.tsx
+++ podcastr_next/src/pages/index.tsx
@@ -1,15 +1,22 @@
 import { useEffect } from "react";
 
-export default function Home() {
-
-  useEffect(() => {
-    fetch('http://localhost:3333/episodes')
-      .then(response => response.json())
-      .then(data => console.log(data));
-  }, [ ]);
-
-
-  return (
-   <h1>Index</h1>
+export default function Home(props) {
+   return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+   
   )
+}
+
+export async function getServerSideProps() {
+    const response = await fetch('http://localhost:3333/episodes')
+    const data = await response.json()
+
+    return {
+      props: {
+        episodes: data,
+      }
+    }
 }
