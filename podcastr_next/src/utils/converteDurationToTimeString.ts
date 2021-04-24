cad0a273e14b1e6a9538d4e07f94775cb2b9e03c
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-04-24 20:22:24 -0300

    criado função para converter a duracao do podcast em string 00:00:00

---
+++ podcastr_next/src/utils/converteDurationToTimeString.ts
@@ -0,0 +1,9 @@
+export function converteDurationToTimeString(duration: number) {
+    const hours = Math.floor(duration / 3600);
+    const minutes = Math.floor((duration % 3600) / 60);
+    const secounds = duration % 60;
+
+    const finalResult = [hours, minutes, secounds]
+        .map(unit => String(unit).padStart(2, '0'))
+        .join(":")
+}
