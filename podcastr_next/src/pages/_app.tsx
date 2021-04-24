d8ca2d1359da79b7cf3d35161c3e706f26e7f54e
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-04-24 20:22:40 -0300

    tslint

--- podcastr_next/src/pages/_app.tsx
+++ podcastr_next/src/pages/_app.tsx
@@ -1,20 +1,20 @@
-import '../styles/global.scss'
+import "../styles/global.scss";
 
-import { Header } from '../components/header'
-import { Player } from '../components/player';
+import { Header } from "../components/header";
+import { Player } from "../components/player";
 
-import styles from '../styles/app.module.scss';
+import styles from "../styles/app.module.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className={styles.wrapper}>
       <main>
-        <Header/>
+        <Header />
         <Component {...pageProps} />
       </main>
       <Player />
-    </div>  
-  )  
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
